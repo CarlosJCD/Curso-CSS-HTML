f2f88cf3994343fcfcb559626d65c3f29a4f8e26
CarlosJCD <carlos.calderon.d@icloud.com>
2023-07-15 22:16:16 -0600

    Videos #203 al #213 - Seccion #19

--- Proyecto_11_cafeteria/gulpfile.js
+++ Proyecto_11_cafeteria/gulpfile.js
@@ -1,14 +1,22 @@
-const { src } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require('gulp');
 
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done) {
     src('src/scss/app.scss')
-        .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(dest('build/css'))
-
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest('build/css'));
     done();
 }
 
+function dev() {
+    watch('src/scss/**/*.scss', css);
+}
+
 exports.css = css;
+exports.dev = dev
+
+exports.default = series(css, dev);
