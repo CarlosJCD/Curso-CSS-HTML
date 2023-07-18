e0ea6f3ad8f140de6dff06e3b213261daeb68675
CarlosJCD <carlos.calderon.d@icloud.com>
2023-07-17 19:12:54 -0600

    Video #253 - Seccion #19 (FIN)

--- Proyecto_11_cafeteria/gulpfile.js
+++ Proyecto_11_cafeteria/gulpfile.js
@@ -4,6 +4,7 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
@@ -13,7 +14,7 @@ function css(done) {
     src('src/scss/app.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(postcss([autoprefixer()]))
+        .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'));
     done();
